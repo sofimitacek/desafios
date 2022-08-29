bb28714686a8f70c0a1f3cfa9ca0036e1e13ae57
Sofía Mitacek <sofiamitacek@gmail.com>
2022-08-29 01:19:46 -0300

    cambios

--- family/models.py
+++ family/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Familia(models.Model):
+    nombre = models.CharField(max_length=128)
+    fecha_nacimiento = models.DateField()
+    altura_m = models.IntegerField()
+    sexo_F_M = models.CharField(max_length=3)
+
+
+    
