1d44b70aee175456fb38506d5d39c893719a56bf
Sofía Mitacek <sofiamitacek@gmail.com>
2022-08-29 01:36:15 -0300

    cambios D:

--- family/urls.py
+++ family/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from family.views import template_1
+from family.views import list_familia
 
 
 urlpatterns = [
-    path('heroes', template_1),
+    path('family', list_familia),
 ]
--- family/views.py
+++ family/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import Template, Context, loader
+from family.models import Familia
 
-# Create your views here.
+
+def list_familia(self):
+    mihtml= open("C:\Users\chofy\OneDrive\Documentos\proyectosAA\familiares\familiares\family\templates\family_list.html")
+    plantilla = Template(mihtml.read())
+    mihtml.close()
+    miContexto = Context()
+    documento = plantilla.render(miContexto)
+
+    return HttpResponse(documento)
